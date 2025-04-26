e1f172f5f491b8fe08696fc51d9a42b7cbb352ec
Lukas Houpt <lh2xs@virginia.edu>
2025-04-26 18:45:05 -0400

    fixed data retrieval

---
+++ how_to_run_limit_checking_report.py
@@ -0,0 +1,2 @@
+# File:"C:\Users\lukas\Documents\Personal Projects\psse_powerflow\how_to_run_limit_checking_report.py", generated on SAT, APR 26 2025  17:37, PSS(R)E Xplore release 36.01.00
+psspy.rate_3(0,1,1,1,1,0,100.0,0)
